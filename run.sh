6fbb6abc18f20fd421b36bf550ffe54a5aa930cf
adumbdinosaur <168869195+adumbdinosaur@users.noreply.github.com>
2025-04-21 15:54:51 +0100

    fix registering (#7)

--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@
 
 # Navigate to the server directory and start the Go server
 echo "Starting the Go server..."
-cd server/cmd/text-app-server || exit
-go run main.go &
+cd server || exit
+go run cmd/text-app-server/main.go &
 SERVER_PID=$!
 
 # Navigate to the React app directory and start the development server
 echo "Starting the React application..."
-cd ../../../site/text-app || exit
+cd ../site/text-app || exit
 yarn
 yarn dev &
 REACT_PID=$!
