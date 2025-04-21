5c48d8d9e7f34258c1093b78cc05a082102c691f
adumbdinosaur <168869195+adumbdinosaur@users.noreply.github.com>
2025-04-21 15:51:44 +0100

    Add run script (#6)
    * lint js
    
    * change title
    
    * add a run script

---
+++ README.md
@@ -0,0 +1,13 @@
+# Running the Applications
+
+To run both the Go server and the React application concurrently, follow these steps:
+
+1. Ensure you have the required dependencies installed:
+   - [Go](https://golang.org/doc/install)
+   - [Node.js and yarn](https://nodejs.org/)
+
+2. Make the `run-apps.sh` script executable (if not already):
+```bash
+chmod +x run-apps.sh
+``` 
+
---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Navigate to the server directory and start the Go server
+echo "Starting the Go server..."
+cd server/cmd/text-app-server || exit
+go run main.go &
+SERVER_PID=$!
+
+# Navigate to the React app directory and start the development server
+echo "Starting the React application..."
+cd ../../../site/text-app || exit
+yarn
+yarn dev &
+REACT_PID=$!
+
+# Wait for both processes to finish
+trap "kill $SERVER_PID $REACT_PID" SIGINT SIGTERM
+wait $SERVER_PID $REACT_PID
